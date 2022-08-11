08fed056b1e028fd5881907acaff836b6c5ae49b
Chris Halogen <christackoms@gmail.com>
2022-08-11 20:02:29 +0000

    Inner and outer folder

---
+++ _print_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * output_string - function that writes string to standard output
+ * @string: the pointer to the string
+ * Return: num of characters writteen to standard output
+ */
+
+int output_string(char *string)
+{
+
+	int count = 0, i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		count += _putchar(string[i]);
+	}
+
+	return (count);
+}
